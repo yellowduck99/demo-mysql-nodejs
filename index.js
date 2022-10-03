234f8c83c9b679dad0d356750160f0da3824edca
sukileung423 <sukileung423@gmail.com>
2022-10-03 10:20:06 +0800

    change to es6

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const mysql = require('mysql2')
-const express = require('express')
+import express from 'express'
+import mysql from 'mysql2'
+
 const PORT = 3002
 const app = express()
 const IP = '' // your DB IP
 const PASSWORD = '' // your DB root pw
 
+
 const conn = mysql.createConnection({ 
-        host:IP,
-        user:'root',
-        password: PASSWORD,
-        database: 'test'
+    host:IP,
+    user:'root',
+    password: PASSWORD,
+    database: 'classicmodels'
 });
 
 
 conn.connect(function(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('connecting success');
+if (err) {
+    console.log(err);
+    return;
+}
+console.log('connecting success');
 });
 
 // link to seperate api file
 app.use((req,res,next) => {
-    req.con = conn;
-    next()
+req.con = conn;
+next()
 })
 
-const route = require(__dirname+'/lib/db.js')
+import route from './lib/db.js'
+
 app.use(route)
 
 app.listen(PORT, ()=> {
-    console.log(`App listening at port ${PORT}`)
+console.log(`App listening at port ${PORT}`)
 
 })
